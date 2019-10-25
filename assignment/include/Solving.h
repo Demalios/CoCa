24d8ce191c254dccf4c9800e9ff36a4b8c5abdf2
Alan Aneas <aaneas@emi.u-bordeaux1.fr>
2019-10-25 14:39:02 +0200

    fusion

--- assignment/include/Solving.h
+++ assignment/include/Solving.h
@@ -18,67 +18,73 @@
 
 /**
  * @brief Generates a formula consisting of a variable representing the fact that @p node of graph number @p number is at position @p position of an accepting path.
+ * Génère une formule consistant en une variable représentant le fait que node du graphe number soit à la position position d'un chemin acceptant.
  * 
- * @param ctx The solver context.
- * @param number The number of the graph.
- * @param position The position in the path.
- * @param k The mysterious k from the subject of this assignment.
- * @param node The node identifier.
- * @return Z3_ast The formula.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param number, The number of the graph. Le numéro du graphe.
+ * @param position, The position in the path. La position du chemin.
+ * @param k, The mysterious k from the subject of this assignment. Le k dans x_i,j,k,q.
+ * @param node, The node identifier. L'identifiant du noeud.
+ * @return Z3_ast, The formula. La formule.
  */
 Z3_ast getNodeVariable(Z3_context ctx, int number, int position, int k, int node);
 
 /**
  * @brief Generates a SAT formula satisfiable if and only if all graphs of @p graphs contain an accepting path of length @p pathLength.
+ * Génère une formule SAT satisfaisable si et seulement si tous les graphes de graphs contiennent un chemin acceptant de longueur pathLength.
  * 
- * @param ctx The solver context.
- * @param graphs An array of graphs.
- * @param numGraphs The number of graphs in @p graphs.
- * @param pathLength The length of the path to check.
- * @return Z3_ast The formula.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param graphs, An array of graphs. Une suite de graphes.
+ * @param numGraphs, The number of graphs in @p graphs. Le numéro (indice ?) du graphe dans graphs.
+ * @param pathLength, The length of the path to check. La longueur du chemin à vérifier.
+ * @return Z3_ast, The formula. La formule.
  */
 Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs, int pathLength);
 
 /**
  * @brief Generates a SAT formula satisfiable if and only if all graphs of @p graphs contain an accepting path of common length.
+ * Génère une formule SAT satisfaisable si et seulement si tous les graphes de graphs contiennent un chemin acceptant de longueur commune.
  * 
- * @param ctx The solver context.
- * @param graphs An array of graphs.
- * @param numGraphs The number of graphs in @p graphs.
- * @return Z3_ast The formula.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param graphs, An array of graphs. Une suite de graphes.
+ * @param numGraphs, The number of graphs in @p graphs. Le numéro (indice ?) du graphe dans graphs.
+ * @return Z3_ast, The formula. La formule.
  */
 Z3_ast graphsToFullFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs);
 
 /**
  * @brief Gets the length of the solution from a given model.
+ * Obtient la longueur de la solution d'un modèle donné.
  * 
- * @param ctx The solver context.
- * @param model A variable assignment.
- * @param graphs An array of graphs.
- * @return int The length of a common simple accepting path in all graphs from @p graphs.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param model, A variable assignment. Une affectation de variables (exemple : x1 = Vrai, x2 = Faux, X3 = Vrai).
+ * @param graphs, An array of graphs. Une liste de graphes.
+ * @return int, The length of a common simple accepting path in all graphs from @p graphs. La longueur d'un chemin simple et acceptant dans tous les graphes de graphs.
  */ 
 int getSolutionLengthFromModel(Z3_context ctx, Z3_model model, Graph *graphs);
 
 /**
  * @brief Displays the paths of length @p pathLength of each graphs in @p graphs described by @p model.
+ * Affiche les chemins de longueur pathLength pour tous les graphes décrits dans model.
  * 
- * @param ctx The solver context.
- * @param model A variable assignment.
- * @param graphs An array of graphs.
- * @param numGraph The number of graphs in @p graphs.
- * @param pathLength The length of path.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param model, A variable assignment. Une affectation de variables (exemple : x1 = Vrai, x2 = Faux, X3 = Vrai).
+ * @param graphs, An array of graphs. Une liste de graphes.
+ * @param numGraph, The number of graphs in @p graphs. Le nombre de graphes dans graphs.
+ * @param pathLength, The length of path. La longueur du chemin.
  */
 void printPathsFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength);
 
 /**
  * @brief Creates the file ("%s-l%d.dot",name,pathLength) representing the solution to the problem described by @p model, or ("result-l%d.dot,pathLength") if name is NULL.
+ * Crée le fichier représentant la solution du problème décrit par model, ou ("result-l%d.dot,pathLength") si name == NULL
  * 
- * @param ctx The solver context.
- * @param model A variable assignment.
- * @param graphs An array of graphs.
- * @param numGraph The number of graphs in @p graphs.
- * @param pathLength The length of path.
- * @param name The name of the output file.
+ * @param ctx, The solver context. Le contexte du solveur.
+ * @param model, A variable assignment. Une affectation de variables (exemple : x1 = Vrai, x2 = Faux, X3 = Vrai).
+ * @param graphs, An array of graphs. Une liste de graphes.
+ * @param numGraph, The number of graphs in @p graphs. Le nombre de graphes dans graphs.
+ * @param pathLength, The length of path. La longueur du chemin.
+ * @param name, The name of the output file. Le nom du fichier en sortie.
  */
 void createDotFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength, char* name);
 
--- assignment/include/Z3Tools.h
+++ assignment/include/Z3Tools.h
@@ -18,48 +18,55 @@
 
 /**
  * @brief Creates a basic Z3 context with basic config (sufficient for this project). Must be freed at end of program with Z3_del_context.
+ * Crée un context Z3 basique avec une configuration basique (suffisante pour ce projet). Doit être libéré à la fin du programme avec Z3_del_context.
  * 
- * @return Z3_context The created context
+ * @return Z3_context, The created context. Le contexte créé.
  */
 Z3_context makeContext(void);
 
 /**
  * @brief Creates a formula containing a single variable whose name is given in parameter. Example mk_bool_var(ctx,"toto") will create the formula «toto». Each call with
  *        same name will produce the same formula (so it can be used to have the same variable in different formulae.)
+ * Crée une formule contenant une seule variable dont le nom est donné en paramètre. Exemple : mk_bool_var(ctx,"toto") créera la formule "toto". Chaque appel avec le même nom
+ * produira la même formule (pour qu'elle soit utilisée pour avoir la même variable dans différentes formules)
  * 
- * @param ctx The context of the solver.
- * @param name The name the variable.
- * @return Z3_ast The formula consisting in the variable.
+ * @param ctx, The context of the solver. Le contexte du solveur.
+ * @param name, The name the variable. Le nom de la variable.
+ * @return Z3_ast, The formula consisting in the variable. La formule consitant en la variable.
  */
 Z3_ast mk_bool_var(Z3_context ctx, const char * name);
 
 /**
  * @brief Tells if a formula is satisfiable, unsatisfiable, or cannot be decided.
+ * Dis si une formule est satisfaisable, non-satisfaisable, ou si on ne peut pas le décider.
  * 
- * @param ctx The context of the solver.
- * @param formula The formula to check.
- * @return Z3_lbool Z3_L_FALSE if @p formula is unsatisfiable, Z3_L_TRUE if @p formula is satisfiable and Z3_L_UNDEF if the solver cannot decide if @p formula is
+ * @param ctx, The context of the solver. Le contexte du solveur.
+ * @param formula, The formula to check. La formule à vérifier.
+ * @return Z3_lbool, Z3_L_FALSE if @p formula is unsatisfiable, Z3_L_TRUE if @p formula is satisfiable and Z3_L_UNDEF if the solver cannot decide if @p formula is
  *         satisfiable or not.
+ * Renvoie Z3_L_FALSE si formula est non-satisfaisable, Z3_L_TRUE si formula est satisfaisable, et Z3_L_UNDEF si le solveur ne peut pas décider si formula est satisfaisable ou non.
  */
 Z3_lbool isFormulaSat(Z3_context ctx, Z3_ast formula);
 
 /**
  * @brief Returns an assignment of variables satisfying the formula if it is satisfiable. Exits the program if the formula is unsatisfiable.
+ * Renvoie une affectation de variables satisfaisant la formule si elle est satisfaisable. Quitte le programme si la formule est non-satisfaisable.
  * 
- * @param ctx The context of the solver.
- * @param formula The formula to get a model from.
- * @return Z3_model An assignment of variable satisfying @p formula.
+ * @param ctx, The context of the solver. Le contexte du solveur.
+ * @param formula, The formula to get a model from. La formule qui nous donne un modèle.
+ * @return Z3_model, An assignment of variable satisfying @p formula. Une affectation de variables satisfaisant la formule (exemple : x1 = Vrai, x2 = Faux, X3 = Vrai).
  */
 Z3_model getModelFromSatFormula(Z3_context ctx, Z3_ast formula);
 
 /**
  * @brief Returns the truth value of the formula @p variable in the variable assignment @p model. Very usefull if @p variable is a formula containing a single variable.
+ * Renvoie la valeur de vérité de la formule nommée variable dans l'affectation model. Très utile si variable est une formule contenant une seule variable.
  * 
- * @param ctx The context of the solver.
- * @param model A variable assignment.
- * @param variable A formula of which we want the truth value.
- * @return true if @p variable is true in @p model
- * @return false otherwise.
+ * @param ctx, The context of the solver. Le contexte du solveur.
+ * @param model, A variable assignment. Une affectation de variables (exemple : x1 = Vrai, x2 = Faux, X3 = Vrai).
+ * @param variable, A formula of which we want the truth value. Une formule dont on veut la valeur de vérité.
+ * @return true, if @p variable is true in @p model. Renvoie vrai si variable est vraie dans model.
+ * @return false, otherwise. Renvoie faux, sinon.
  */
 bool valueOfVarInModel(Z3_context ctx, Z3_model model, Z3_ast variable);
 
