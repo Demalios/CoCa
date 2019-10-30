58b01d8287583afad0af501eef341d5eee2564f8
Alan Aneas <aaneas@emi.u-bordeaux1.fr>
2019-10-30 10:18:31 +0100

    main

--- assignment/src/main.c
+++ assignment/src/main.c
@@ -1,6 +1,19 @@
 /* A compléter */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "../include/Parsing.h"
+#include "../include/Graph.h"
+#include <z3.h>
+#include "../include/Z3Tools.h"
+#include <string.h>
+#include <math.h>
 
 int main(int argc, char* argv[]){
-    
+    Graph graph;
+    graph = getGraphFromFile("G1.dot");
+    //graph = getGraphFromFile(argv[1]);
+    printGraph(graph);
+    void deleteGraph(graph);
     return 1;
 }
