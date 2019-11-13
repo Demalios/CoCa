e7a5431917a50a1885a84e4531fcf02626f6bff4
Alexis Cataldi <acataldi@emi.u-bordeaux1.fr>
2019-11-13 10:52:52 +0100

    réduction code

--- assignment/src/main.c
+++ assignment/src/main.c
@@ -10,21 +10,56 @@
 #include <math.h>
 #include "Solving.h"
 
+bool DEFAULT_DISP_G = false;
+bool DEFAULT_DISP_F = false;
+bool DEFAULT_DISP_f = false;
+bool DEFAULT_DISP_P = false;
+bool DEFAULT_DISP_s = false;
+    
+void SAT(Z3_context ctx, Z3_ast formula, Graph * graphs, int numGraph){
+    Z3_lbool isSat = isFormulaSat(ctx,formula);
+    switch (isSat){
+        case Z3_L_FALSE:
+            printf("Non\n");
+            break;
+
+        case Z3_L_UNDEF:
+            printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
+            break;
+
+        case Z3_L_TRUE:
+            if(DEFAULT_DISP_F){
+                printf("%s \n",Z3_ast_to_string(ctx,formula));
+            }
+            printf("Oui\n");
+            
+            if(DEFAULT_DISP_P){
+                Z3_model model = getModelFromSatFormula( ctx, formula);
+                int k = getSolutionLengthFromModel(ctx,model,graphs);
+                printPathsFromModel( ctx, model, graphs, numGraph, k);
+            }
+            if(DEFAULT_DISP_f){
+                Z3_model model = getModelFromSatFormula(ctx, formula);
+                int k = getSolutionLengthFromModel(ctx,model,graphs);
+                createDotFromModel(ctx, model, graphs, numGraph, k, "result");
+            }
+            break;
+    }
+}
+
 int main(int argc, char* argv[]){
-    bool DEFAULT_DISP_G = false;
-    bool DEFAULT_DISP_F = false;
-    bool DEFAULT_DISP_f = false;
-    bool DEFAULT_DISP_P = false;
+    
     int numArg = 1;
 
     for(int i = 1 ; i < argc ; i++ ){
         if(strcmp(argv[i],"-h") == 0){
             printf("You can use the following argument :\n");
-            printf("-h : To display this message\n");
-            printf("-F : To display the computed formula\n");
-            printf("-v : To display the parsed graphs\n");
-            printf("-t : To display a valid path for each graph of size found in common\n");
-            printf("-f : To create a .dot file in the sol folder with the valid paths written in them\n");
+            printf("-h : Displays this help\n");
+            printf("-F : Displays the formula computed (obviously not in this version, but you should really display it in your code)\n");
+            printf("-v : Activate verbose mode (displays parsed graphs)\n");
+            printf("-t : Displays the paths found on the terminal [if not present, only displays the existence of the path].\n");
+            printf("-f : Writes the result with colors in a .dot file. See next option for the name. These files will be produced in the folder 'sol'.\n");
+            printf("-s : Tests separately all formula by depth [if not present: uses the global formula]\n");
             numArg ++;
         }
         if(strcmp(argv[i],"-F") == 0){
@@ -43,28 +78,13 @@ int main(int argc, char* argv[]){
             DEFAULT_DISP_f = true;
             numArg ++;
         }
+        if(strcmp(argv[i],"-s") == 0){
+            DEFAULT_DISP_s = true;
+            numArg ++;
+        }
     }
-    /*
-    if(argc>3 && (strcmp(argv[1],"-h") == 0 || strcmp(argv[2],"-h") == 0 || strcmp(argv[3],"-h") == 0)){
-        printf("You can use the following argument :\n");
-        printf("-h : To display this message\n");
-        printf("-F : To display the computed formula\n");
-        printf("-v : To display the parsed graphs\n");
-        numArg ++;
-    }
-    if(argc>3 && (strcmp(argv[1],"-F") == 0 || strcmp(argv[2],"-F") == 0 || strcmp(argv[3],"-F") == 0)){
-        printf("ici\n");
-        DEFAULT_DISP_F = true;
-        numArg ++;
-    }
-    if(argc>3 && (strcmp(argv[1],"-v") == 0 || strcmp(argv[2],"-v") == 0 || strcmp(argv[3],"-v") == 0)){
-        DEFAULT_DISP_G = true;
-        numArg ++;
-    }
-    */
     int numGraph = argc-numArg;
     if (numGraph < 1){
-        //usage
         printf("Please enter at least one graph, for example: ./equalPath -h graphs/assignment-instance/G1.dot graphs/assignment-instance/triangle.dot\n");
         return EXIT_SUCCESS;
     }
@@ -75,38 +95,10 @@ int main(int argc, char* argv[]){
             printGraph(graph[i]);
         }
     }
-    //graph = getGraphFromFile(argv[1]);
-    //printGraph(graph[0]);
     Z3_context ctx = makeContext();
-    // Pour un k précis
-    /*
-    Z3_ast formula = graphsToPathFormula(ctx, graph, numGraph, pathLength);
-    Z3_lbool isSat = isFormulaSat(ctx,formula);
-    switch (isSat){
-        case Z3_L_FALSE:
-            printf("%s is not satisfiable.\n",Z3_ast_to_string(ctx,formula));
-            break;
-
-        case Z3_L_UNDEF:
-            printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
-            break;
-
-        case Z3_L_TRUE:
-            printf("%s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
-            Z3_model model = getModelFromSatFormula( ctx, formula);
-            printPathsFromModel( ctx, model, graph, numGraph, pathLength);
-            /*Z3_model model = getModelFromSatFormula(ctx,absurd);
-            printf("Model obtained for %s:\n",Z3_ast_to_string(ctx,absurd));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,x),valueOfVarInModel(ctx,model,x));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,y),valueOfVarInModel(ctx,model,y));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,negX),valueOfVarInModel(ctx,model,negX));
-            
-            break;
-    }
-    */
-    // Pour tout k entre 0 et kmax
-    
     Z3_ast formula = graphsToFullFormula(ctx,graph,numGraph);
+    SAT(ctx,formula,graph,numGraph);
+    /*
     Z3_lbool isSat = isFormulaSat(ctx,formula);
     switch (isSat){
         case Z3_L_FALSE:
@@ -128,22 +120,14 @@ int main(int argc, char* argv[]){
                 int k = getSolutionLengthFromModel(ctx,model,graph);
                 printPathsFromModel( ctx, model, graph, numGraph, k);
             }
-
             if(DEFAULT_DISP_f){
                 Z3_model model = getModelFromSatFormula(ctx, formula);
                 int k = getSolutionLengthFromModel(ctx,model,graph);
                 createDotFromModel(ctx, model, graph, numGraph, k, "result");
             }
-            
-            /*Z3_model model = getModelFromSatFormula(ctx,absurd);
-            printf("Model obtained for %s:\n",Z3_ast_to_string(ctx,absurd));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,x),valueOfVarInModel(ctx,model,x));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,y),valueOfVarInModel(ctx,model,y));
-            printf("    The value of %s is %d\n",Z3_ast_to_string(ctx,negX),valueOfVarInModel(ctx,model,negX));
-            */
             break;
     }
-
+    */
     for(int i = 0 ; i < numGraph ; i++){
         deleteGraph(graph[i]);
     }
